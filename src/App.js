ba6a62ce2eaecf64d4c3adc8a81fa43c966150c9
Vignesh Jagan <vigneshjagan@Vigneshs-MacBook-Pro.local>
2019-03-19 15:09:20 -0400

    change

--- src/App.js
+++ src/App.js
@@ -8,6 +8,12 @@ class App extends Component {
       <div className="App">
            <h2>Test</h2>
            <p>test p</p>
+           <p>Adding more lines</p>
+
+           <ol>
+             <li>1OL</li>
+             <li>20L</li>
+           </ol>
       </div>
     );
   }
